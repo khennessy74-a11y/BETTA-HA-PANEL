d75f19db72e0fcd1e9cf464bd7ef0d31323c817e
khennessy74-a11y <khennessy74@gmail.com>
2026-09-17 16:25:20 +0100

    Add 'timer' to discovery domain checks

--- main/ha/ha_client.c
+++ main/ha/ha_client.c
@@ -1389,7 +1389,8 @@ static bool ha_client_discovery_domain_supported(const char *domain)
            strcmp(domain, "vacuum") == 0 ||
            strcmp(domain, "image") == 0 ||
            strcmp(domain, "script") == 0 ||
-           strcmp(domain, "scene") == 0;
+           strcmp(domain, "scene") == 0 ||
+           strcmp(domain, "timer") == 0;
 }
 
 static const char *ha_client_discovery_domain_or_default(const char *domain)
