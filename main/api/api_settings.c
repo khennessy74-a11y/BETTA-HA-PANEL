fded1272b871274408c7f314180aaee13e1509f5
khennessy74-a11y <khennessy74@gmail.com>
2026-09-22 13:26:37 +0100

    Expose night mode settings API

--- main/api/api_settings.c
+++ main/api/api_settings.c
@@ -254,6 +254,10 @@ esp_err_t api_settings_get_handler(httpd_req_t *req)
 
     cJSON_AddStringToObject(ui, "language", settings->ui_language);
     cJSON_AddNumberToObject(ui, "brightness_percent", settings->display_brightness_percent);
+    cJSON_AddNumberToObject(ui, "night_brightness_percent", settings->display_night_brightness_percent);
+    cJSON_AddBoolToObject(ui, "night_mode_auto", settings->display_night_mode_auto);
+    cJSON_AddNumberToObject(ui, "night_start_hour", settings->display_night_start_hour);
+    cJSON_AddNumberToObject(ui, "day_start_hour", settings->display_day_start_hour);
     cJSON_AddItemToObject(root, "ui", ui);
 
     cJSON_AddBoolToObject(root, "ok", true);
@@ -436,6 +440,13 @@ esp_err_t api_settings_put_handler(httpd_req_t *req)
             ui, "language", settings->ui_language, sizeof(settings->ui_language), &invalid_type, &too_long);
         (void)update_int_setting(
             ui, "brightness_percent", &settings->display_brightness_percent, 0, 100, &invalid_type);
+        (void)update_int_setting(
+            ui, "night_brightness_percent", &settings->display_night_brightness_percent, 0, 100, &invalid_type);
+        (void)update_bool_setting(ui, "night_mode_auto", &settings->display_night_mode_auto, &invalid_type);
+        (void)update_int_setting(
+            ui, "night_start_hour", &settings->display_night_start_hour, 0, 23, &invalid_type);
+        (void)update_int_setting(
+            ui, "day_start_hour", &settings->display_day_start_hour, 0, 23, &invalid_type);
     }
 
     (void)update_string_setting(
